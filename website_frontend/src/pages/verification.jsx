76b841481fb73d89588f2b51aca8034e478d411d
Tanisha Dixit <dixittanisha02@gmail.com>
2025-09-24 20:02:11 +0530

    feat: adds routing

---
+++ website_frontend/src/pages/verification.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Verification = () => (
+  <div className="page-blank">
+    <h2>Verification</h2>
+    <p>This page is under construction.</p>
+  </div>
+);
+
+export default Verification;
